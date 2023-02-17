91111df80a153a2d2a90328d9f233b8db0e96ff7
wxluoyouda <ydlro@qq.com>
2023-02-17 15:20:03 +0800

    完善代码

--- 7day-gin/main.go
+++ 7day-gin/main.go
@@ -1,16 +1,62 @@
 package main
 
-import (
-	"fmt"
+/*
+(1)
+$ curl -i http://localhost:9999/
+HTTP/1.1 200 OK
+Date: Mon, 12 Aug 2019 16:52:52 GMT
+Content-Length: 18
+Content-Type: text/html; charset=utf-8
+<h1>Hello Gee</h1>
 
-	"github.com/disiqueira/gotree"
+(2)
+$ curl "http://localhost:9999/hello?name=geektutu"
+hello geektutu, you're at /hello
+
+(3)
+$ curl "http://localhost:9999/login" -X POST -d 'username=geektutu&password=1234'
+{"password":"1234","username":"geektutu"}
+
+(4)
+$ curl "http://localhost:9999/xxx"
+404 NOT FOUND: /xxx
+*/
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"gee"
 )
 
 func main() {
-	artist := gotree.New("Pantera")
-	album := artist.Add("Far Beyond Driven")
-	album.Add("5 minutes Alone")
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
 
-	fmt.Println(artist.Print())
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
 }
 
+func onlyForV2() gee.HandlerFunc {
+	return func(c *gee.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
